c7646ba8c469816f2b57dcadb441cf92eb28837a
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-04-17 18:49:50 -0700

    Wrote method to pass tests

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,8 +5,23 @@
 # ....
 # e.g. 6th fibonacci number is 8
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n), where n is the value of the integer passed into the method. The loop will run n - 1 times, so the number of times is linearally related to the value of n.
+# Space complexity: O(1) because regardless of the value of the integer passed in, the amount of memory used stays the same.
 def fibonacci(n)
-  raise NotImplementedError
+  if n.class != Integer || n < 0
+    raise ArgumentError
+  elsif n == 0
+    return 0
+  end
+
+  result = 1
+  a = 1
+  b = 0
+  (n - 1).times do
+    result = a + b
+    b = a
+    a = result
+  end
+
+  return result
 end
